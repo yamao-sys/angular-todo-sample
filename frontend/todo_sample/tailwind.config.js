ee5efa579d3341688ac5638804ce5fdb89484ced
yamao-sys <yamaopanku@gmail.com>
2024-09-23 19:46:12 +0900

    Tailwind CSSの初期化

---
+++ frontend/todo_sample/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
