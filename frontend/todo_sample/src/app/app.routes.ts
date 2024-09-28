4f109c0a6d9ff7bdc912f16e38d836b380e7c309
yamao-sys <yamaopanku@gmail.com>
2024-09-28 16:51:41 +0900

    会員登録ページの作成

--- frontend/todo_sample/src/app/app.routes.ts
+++ frontend/todo_sample/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth/sign_up',
+    component: SignUpComponent,
+    title: '会員登録',
+  },
+];
---
+++ frontend/todo_sample/src/app/pages/sign-up/sign-up.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SignUpDto, ValidateSignUpResponseDto } from '../../../generated/auth/@types';
+import { postSignUp, postValidateSignUp } from 'apis/authApi';
+import { CommonModule } from '@angular/common';
+
+type PhaseType = 'input' | 'confirmation' | 'thanks';
+
+const INITIAL_VALIDATION_ERRORS = {
+  errors: {
+    name: [],
+    email: [],
+    password: [],
+    passwordConfirm: [],
+  },
+};
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css',
+})
+export class SignUpComponent {
+  // 画面のフェーズ
+  phase: PhaseType = 'input';
+
+  signUpInputs: SignUpDto = {
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  };
+
+  validationErrors: ValidateSignUpResponseDto = INITIAL_VALIDATION_ERRORS;
+
+  togglePhase(toPhase: PhaseType) {
+    this.phase = toPhase;
+  }
+
+  async handleValidateSignUp() {
+    // NOTE: バリデーションエラーをリセット
+    this.validationErrors = INITIAL_VALIDATION_ERRORS;
+
+    try {
+      const response = await postValidateSignUp(this.signUpInputs);
+
+      // NOTE: バリデーションエラーがなければ、確認画面へ遷移
+      if (Object.keys(response.errors).length === 0) {
+        this.togglePhase('confirmation');
+        return;
+      }
+
+      // NOTE: バリデーションエラーの格納と入力パスワードのリセット
+      this.validationErrors = response;
+
+      this.signUpInputs.password = '';
+      this.signUpInputs.passwordConfirm = '';
+    } catch (error) {
+      // TODO: エラーハンドリング
+      console.log(error);
+    }
+  }
+
+  async handleSignUp() {
+    try {
+      await postSignUp(this.signUpInputs);
+      this.togglePhase('thanks');
+    } catch (error) {
+      // TODO: エラーハンドリング
+      console.log(error);
+    }
+  }
+}
