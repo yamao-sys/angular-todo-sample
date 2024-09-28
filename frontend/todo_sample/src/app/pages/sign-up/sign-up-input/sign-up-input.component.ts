a4c1ea9d81682536de6840a621d2de6815fcd786
yamao-sys <yamaopanku@gmail.com>
2024-09-28 17:48:19 +0900

    共通コンポーネント(ValidationErrors)の作成

--- frontend/todo_sample/src/app/pages/sign-up/sign-up-input/sign-up-input.component.ts
+++ frontend/todo_sample/src/app/pages/sign-up/sign-up-input/sign-up-input.component.ts
@@ -3,12 +3,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BaseFormBoxComponent } from 'app/components/base-form-box/base-form-box.component';
 import { InputFormComponent } from 'app/components/input-form/input-form.component';
+import { ValidationErrorsComponent } from 'app/components/validation-errors/validation-errors.component';
 import { SignUpDto, ValidateSignUpResponseDto } from 'generated/auth/@types';
 
 @Component({
   selector: 'app-sign-up-input',
   standalone: true,
-  imports: [FormsModule, CommonModule, BaseFormBoxComponent, InputFormComponent],
+  imports: [
+    FormsModule,
+    CommonModule,
+    BaseFormBoxComponent,
+    InputFormComponent,
+    ValidationErrorsComponent,
+  ],
   templateUrl: './sign-up-input.component.html',
   styleUrl: './sign-up-input.component.css',
 })
