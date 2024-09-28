049b0bc86e7afb75769b677518a48c8d08a851af
yamao-sys <yamaopanku@gmail.com>
2024-09-28 16:50:25 +0900

    バックエンドのCORS対応

--- api_server/app/src/main.ts
+++ api_server/app/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number(process.env.PORT) || 3000;
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:4200',
     credentials: true,
   });
   await app.listen(port, '0.0.0.0');
