ea5c0ba22fb13093570244f049030c29e7523ef7
yamao-sys <yamaopanku@gmail.com>
2024-09-29 10:21:42 +0900

    TODOのAPIのI/F作成

---
+++ frontend/todo_sample/src/apis/todoApi.ts
@@ -0,0 +1,35 @@
+import aspida, { FetchConfig } from '@aspida/fetch';
+import api from 'generated/todos/$api';
+import { CreateTodoDto, UpdateTodoDto } from 'generated/todos/@types';
+
+const getTodoApiClient = (options?: FetchConfig) => {
+  const baseFetchConditions = {
+    baseURL: import.meta.env['NG_APP_BASE_API_URL'],
+    throwHttpErrors: true,
+  };
+
+  return api(aspida(fetch, { ...baseFetchConditions, ...(options || {}) }));
+};
+
+export const fetchTodos = async () => {
+  return await getTodoApiClient().todos.$get();
+};
+
+export const postCreateTodo = async (data: CreateTodoDto) => {
+  return await getTodoApiClient().todos.post({
+    body: {
+      title: data.title,
+      content: data.content,
+    },
+  });
+};
+
+export const updateTodo = async (id: string, inputTodo: UpdateTodoDto) => {
+  return await getTodoApiClient().todos._id(id).$patch({
+    body: inputTodo,
+  });
+};
+
+export const deleteTodo = async (id: string) => {
+  return await getTodoApiClient().todos._id(id).$delete();
+};
